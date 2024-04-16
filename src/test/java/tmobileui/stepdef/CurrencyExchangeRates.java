9db5327b542b5c4c94a3b98a2b5a74f00e34279d
Krzysztof Konstantinow <krzysztof.konstantinow@gmail.com>
2024-04-16 23:12:36 +0200

    changes to feature file

--- src/test/java/tmobileui/stepdef/CurrencyExchangeRates.java
+++ src/test/java/tmobileui/stepdef/CurrencyExchangeRates.java
@@ -15,47 +15,47 @@ public class CurrencyExchangeRates {
     private Response response;
     private JsonPath jsonPath;
 
-    @Given("^I have the API endpoint to get exchange rates$")
-    public void i_have_the_API_endpoint_to_get_exchange_rates() {
+    @Given("^API endpoint to get exchange rates is known$")
+    public void api_endpoint_to_get_exchange_rates_is_known() {
         RestAssured.baseURI = "http://api.nbp.pl";
         RestAssured.basePath = "/api/exchangerates/tables/A";
     }
 
-    @When("^I make a GET request to get exchange rates$")
-    public void i_make_a_GET_request_to_get_exchange_rates() {
+    @When("^GET request to get exchange rates is made$")
+    public void get_request_to_get_exchange_rates_is_made() {
         response = get();
         jsonPath = response.jsonPath();
     }
 
-    @Then("^I should see the response code (\\d+)$")
-    public void i_should_see_the_response_code(int expectedStatusCode) {
+    @Then("^The response code (\\d+) should be displayed$")
+    public void the_response_code_should_be_displayed(int expectedStatusCode) {
         assertEquals(expectedStatusCode, response.getStatusCode());
         System.out.println("Response code is: " + response.getStatusCode());
     }
 
-    @Then("^I should display exchange rate for currency with code (\\w+)$")
-    public void i_should_display_exchange_rate_for_currency_with_code(String currencyCode) {
+    @Then("^Exchange rate for currency with code (\\w+) should be displayed$")
+    public void exchange_rate_for_currency_with_code_should_be_displayed(String currencyCode) {
 
         String exchangeRate = jsonPath.getString("[0].rates.find{it.code == '" + currencyCode + "'}.mid");
         assertNotNull(exchangeRate);
         System.out.println("Exchange rate for " + currencyCode + " is: " + exchangeRate);
     }
 
-    @Then("^I should display exchange rate for currency with name (.*)$")
-    public void i_should_display_exchange_rate_for_currency_with_name(String currencyName) {
+    @Then("^Exchange rate for currency with name (.*) should be displayed$")
+    public void exchange_rate_for_currency_with_name_should_be_displayed(String currencyName) {
         String exchangeRate = jsonPath.getString("[0].rates.find {it.currency == '" + currencyName + "' }.mid");
         assertNotNull(exchangeRate);
         System.out.println("Exchange rate for " + currencyName + ": " + exchangeRate);
     }
 
-    @Then("^I should display currencies with exchange rate over: (\\d+)$")
-    public void i_should_display_currencies_with_exchange_rate_over(int minValue) {
+    @Then("^Currencies with exchange rate over: (\\d+) should be displayed$")
+    public void currencies_with_exchange_rate_over_should_be_displayed(int minValue) {
         System.out.println("Currencies with exchange rate over " + minValue + ":");
         jsonPath.getString("[0].rates.findAll { it.mid > " + minValue + " }.each { println it.currency }");
     }
 
-    @Then("^I should display currencies with exchange rate under: (\\d+)$")
-    public void i_should_display_currencies_with_exchange_rate_under(int maxValue) {
+    @Then("^Currencies with exchange rate under: (\\d+) should be displayed$")
+    public void currencies_with_exchange_rate_under_should_be_displayed(int maxValue) {
         System.out.println("Currencies with exchange rate under " + maxValue + ":");
         jsonPath.getString("[0].rates.findAll { it.mid < " + maxValue + " }.each { println it.currency }");
     }
--- src/test/resources/featureFiles/Zadanie2.feature
+++ src/test/resources/featureFiles/Zadanie2.feature
@@ -1,13 +1,13 @@
 Feature: Zadanie2
 
   Scenario Outline: Currency exchange rates
-    Given I have the API endpoint to get exchange rates
-    When I make a GET request to get exchange rates
-    Then I should see the response code <expectedStatusCode>
-    And I should display exchange rate for currency with code <currencyCode>
-    And I should display exchange rate for currency with name <currencyName>
-    And I should display currencies with exchange rate over: <minValue>
-    And I should display currencies with exchange rate under: <maxValue>
+    Given API endpoint to get exchange rates is known
+    When GET request to get exchange rates is made
+    Then The response code <expectedStatusCode> should be displayed
+    And Exchange rate for currency with code <currencyCode> should be displayed
+    And Exchange rate for currency with name <currencyName> should be displayed
+    And Currencies with exchange rate over: <minValue> should be displayed
+    And Currencies with exchange rate under: <maxValue> should be displayed
 
     Examples:
       | expectedStatusCode | currencyCode | currencyName       | minValue | maxValue |
